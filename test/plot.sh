b68c1bb2ee8a37e791a2131acfdaf75b9ec5c685
Federico <federicoscutti@gmail.com>
2016-06-21 02:46:32 +0000

    fake estimate flag

--- test/plot.sh
+++ test/plot.sh
@@ -2,7 +2,5 @@
 
 # Strings are passed to the script but this is redundant!
 
-python ../ssdilep/scripts/merge.py --var="mulead_pt" --reg="FAKESVR4_NUM" --lab="TEST REGION" --icut="6" --input="/imports/rcs5_data/fscutti/ssdilep/HistMonVRTwoMu" --output="./" --makeplot=True
-
-
+python ../ssdilep/scripts/merge.py --var="mumu_mulead_pt" --reg="FAKESVR2_NUM" --lab="TEST REGION" --icut="5" --input="/coepp/cephfs/mel/fscutti/ssdilep/HistQUOKKA" --output="./" --makeplot=False --fakest="Subtraction"
 
