3012752a91fadf2fbd5c14825372cc5fdcdc38b2
Steven Keyte <skeyte@vm-115-146-84-190.melbourne.rc.nectar.org.au>
2016-09-28 04:14:34 +0000

    hist.sh updated

--- test/hist.sh
+++ test/hist.sh
@@ -2,11 +2,12 @@
 
 ## Batch
 
-INPATH="/coepp/cephfs/mel/fscutti/ssdilep/menu_singlemu/merged/nominal"
+INPATH="/coepp/cephfs/mel/fscutti/ssdilep/HIGG3D3_v2_p2689/merged/nominal"
 INSCRIPT="../ssdilep/run"
-SCRIPT="j.plotter_SigEff.py"
+SCRIPT="j.plotter_FFEff_Loose.py"
 
 #python ${INSCRIPT}/${SCRIPT} --input ${INPATH}/physics_Main_00276262.root --sampletype="data" #--config="sys:FF_DN"
-python ${INSCRIPT}/${SCRIPT} --input ${INPATH}/Zmumu_Pt1000_2000_CVetoBVeto.root --sampletype="mc" #--config="sys:FF_DN"
+#python ${INSCRIPT}/${SCRIPT} --input ${INPATH}/Sherpa_NNPDF30NNLO_Zmumu_Pt70_140_BFilter.root --sampletype="mc" --events=500 #--config="sys:FF_DN"
+python ${INSCRIPT}/${SCRIPT} --input ${INPATH}/physics_Main_00300687.root --sampletype="data" --events=500 #--config="sys:FF_DN"
 
 
