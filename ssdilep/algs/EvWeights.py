abd61235ef9ec3352864762c7ea61d0d99dff313
Federico <federicoscutti@gmail.com>
2016-10-07 01:39:16 +0000

    Loose WP for fail Iso in trigEFF

--- ssdilep/algs/EvWeights.py
+++ ssdilep/algs/EvWeights.py
@@ -98,19 +98,19 @@ class MuTrigSF(pyframe.core.Algorithm):
     """
     #__________________________________________________________________________
     def __init__(self, name="MuTrigSF",
-            #mu_index      = None,
             is_single_mu   = None,
             is_di_mu       = None,
-            mu_trig_level  = None,
+            mu_iso         = None,
+            mu_reco        = None,
             mu_trig_chain  = None,
             key            = None,
             scale          = None,
             ):
         pyframe.core.Algorithm.__init__(self, name=name)
-        #self.mu_index         = mu_index
         self.is_single_mu      = is_single_mu
         self.is_di_mu          = is_di_mu
-        self.mu_trig_level     = mu_trig_level
+        self.mu_iso            = mu_iso
+        self.mu_reco           = mu_reco
         self.mu_trig_chain     = mu_trig_chain
         self.key               = key
         self.scale             = scale
@@ -118,18 +118,14 @@ class MuTrigSF(pyframe.core.Algorithm):
         assert key, "Must provide key for storing mu reco sf"
     #_________________________________________________________________________
     def initialize(self):
-      allowed_levels = [
-          "Loose_Loose",
-          "Loose_FixedCutTightTrackOnly",
-          ]
-      assert self.mu_trig_level in allowed_levels, "ERROR: mu trig iso level %s is invalid. Check configuration!!!" % self.mu_trig_level
+      if "Not" in self.mu_iso: self.mu_iso = "Loose"
+      if "Not" in self.mu_reco: self.mu_reco = "Loose"
 
     #_________________________________________________________________________
     def execute(self, weight):
         trig_sf=1.0
         if "mc" in self.sampletype: 
           muons = self.store['muons']
-          mu_pairs = self.store['mu_pairs']
           
           num = 1.0 
           den = 1.0
@@ -137,25 +133,10 @@ class MuTrigSF(pyframe.core.Algorithm):
           if self.is_single_mu:
             for i,m in enumerate(muons):
               if m.isTruthMatchedToMuon: 
-                #if not m.isMatchedToTrigChain(): continue
-                sf  = getattr(m,"_".join(["TrigEff","SF",str(self.mu_trig_level)])).at(0)
-                eff = getattr(m,"_".join(["TrigMCEff",str(self.mu_trig_level)])).at(0)
+                sf  = getattr(m,"_".join(["TrigEff","SF",self.mu_reco,self.mu_iso])).at(0)
+                eff = getattr(m,"_".join(["TrigMCEff",self.mu_reco,self.mu_iso])).at(0)
                 num *= 1 - sf * eff
                 den *= 1 - eff
-          
-          elif self.is_di_mu:
-            for i,mp in enumerate(mu_pairs):
-              if mp.lead.isTruthMatchedToMuon and mp.sublead.isTruthMatchedToMuon: 
-                #if not m.isMatchedToTrigChain(): continue
-                
-                sf_lead  = getattr(mp.lead,"_".join(["TrigEff","SF",str(self.mu_trig_level)])).at(0)
-                eff_lead = getattr(mp.lead,"_".join(["TrigMCEff",str(self.mu_trig_level)])).at(0)
-                
-                sf_sublead  = getattr(mp.sublead,"_".join(["TrigEff","SF",str(self.mu_trig_level)])).at(0)
-                eff_sublead = getattr(mp.sublead,"_".join(["TrigMCEff",str(self.mu_trig_level)])).at(0)
-               
-                num *= 1 - sf_lead * sf_sublead * eff_lead * eff_sublead
-                den *= 1 - eff_lead * eff_sublead
           
           else: pass  
           
