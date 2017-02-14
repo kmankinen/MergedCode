3879169b4bf3d52a89b1aeee5f79ae3d0d6153ef
Katja Hannele Karppinen <katja.hannele.karppinen@cern.ch>
2017-02-14 18:22:34 +0100

    Added electron histos to Main_hists.py and histconfig.py

--- ssdilep/hists/Main_hists.py
+++ ssdilep/hists/Main_hists.py
@@ -19,6 +19,11 @@ hist_list.append(h_muons_dphi)
 hist_list.append(h_muons_deta)
 hist_list.append(h_muons_mVis)
 hist_list.append(h_muons_mTtot)
+hist_list.append(h_electrons_chargeprod)
+hist_list.append(h_electrons_dphi)
+hist_list.append(h_electrons_deta)
+hist_list.append(h_electrons_mVis)
+hist_list.append(h_electrons_mTtot)
 
 
 # -------
@@ -45,6 +50,30 @@ hist_list.append(h_musublead_trkz0)
 hist_list.append(h_musublead_trkz0sintheta)
 hist_list.append(h_musublead_ptvarcone30)
 
+
+# -------
+# electrons
+# -------
+
+# elelead
+hist_list.append(h_elelead_pt)
+hist_list.append(h_elelead_eta)
+hist_list.append(h_elelead_phi)
+hist_list.append(h_elelead_trkd0)
+hist_list.append(h_elelead_trkd0sig)
+hist_list.append(h_elelead_trkz0)
+hist_list.append(h_elelead_trkz0sintheta)
+hist_list.append(h_elelead_ptvarcone30)
+
+# elesublead
+hist_list.append(h_elesublead_pt)
+hist_list.append(h_elesublead_eta)
+hist_list.append(h_elesublead_phi)
+hist_list.append(h_elesublead_trkd0)
+hist_list.append(h_elesublead_trkd0sig)
+hist_list.append(h_elesublead_trkz0)
+hist_list.append(h_elesublead_trkz0sintheta)
+hist_list.append(h_elesublead_ptvarcone30)
 
 # -------
 # MET
--- ssdilep/hists/histconfig.py
+++ ssdilep/hists/histconfig.py
@@ -152,6 +152,67 @@ h_scdphi  = Hist1D( hname  = "h_scdphi",
                             )
 
 
+h_electrons_chargeprod  = Hist1D( hname  = "h_electrons_chargeprod",
+                              xtitle = "q(e_{lead}) #timesq (e_{sublead})",
+                              ytitle = "Events", 
+                              nbins  = 4,
+                              xmin   = -2,
+                              xmax   = 2,
+                              dir    = "event",
+                              vexpr  = "self.store['charge_product']",
+                            )
+
+h_electrons_dphi  = Hist1D( hname  = "h_electrons_dphi",
+                              xtitle = "#Delta#phi(e_{lead},e_{sublead})",
+                              ytitle = "Events", 
+                              nbins  = 64,
+                              xmin   = -3.2,
+                              xmax   = 3.2,
+                              dir    = "event",
+                              vexpr  = "self.store['electrons_dphi']",
+                            )
+
+h_electrons_delta  = Hist1D( hname  = "h_electrons_delta",
+                              xtitle = "#Delta#eta(e_{lead},e_{sublead})",
+                              ytitle = "Events", 
+                              nbins  = 50,
+                              xmin   = -2.5,
+                              xmax   = 2.5,
+                              dir    = "event",
+                              vexpr  = "self.store['electrons_deta']",
+                            )
+
+h_electrons_mVis  = Hist1D( hname  = "h_electrons_mVis",
+                              xtitle = "m_{vis}(e_{lead},e_{sublead}) [GeV]",
+                              ytitle = "Events / (1 GeV)", 
+                              nbins  = 2000,
+                              xmin   = 0.0,
+                              xmax   = 2000.,
+                              dir    = "event",
+                              vexpr  = "self.store['mVis']/GeV",
+                            )
+
+h_electrons_mTtot  = Hist1D( hname  = "h_electrons_mTtot",
+                              xtitle = "m^{tot}_{T}(e_{lead},e_{sublead}) [GeV]",
+                              ytitle = "Events / (1 GeV)", 
+                              nbins  = 2000,
+                              xmin   = 0.0,
+                              xmax   = 2000.,
+                              dir    = "event",
+                              vexpr  = "self.store['mTtot']/GeV",
+                            )
+
+h_ejet_dphi  = Hist1D( hname  = "h_ejet_dphi",
+                              xtitle = "#Delta#phi(e_{lead},jet_{lead})",
+                              ytitle = "Events", 
+                              nbins  = 64,
+                              xmin   = -3.2,
+                              xmax   = 3.2,
+                              dir    = "event",
+                              vexpr  = "self.store['ejet_dphi']",
+)
+
+
 # -------
 # jets
 # -------
@@ -502,5 +563,339 @@ h_mulead_pt_jetlead_pt  = Hist2D( hname      = "h_mulead_pt_jetlead_pt",
                               dir     = "event",
                               vexpr   = " self.store['muons'][0].tlv.Pt() / GeV , self.store['jets'][0].tlv.Pt() / GeV",
                           )
+
+#-----------
+# electrons
+#-----------
+
+h_electrons_chargeprod  = Hist1D( hname  = "h_electrons_chargeprod",
+                              xtitle = "q(e_{lead}) #timesq (e_{sublead})",
+                              ytitle = "Events", 
+                              nbins  = 4,
+                              xmin   = -2,
+                              xmax   = 2,
+                              dir    = "event",
+                              vexpr  = "self.store['charge_product']",
+                            )
+
+h_electrons_dphi  = Hist1D( hname  = "h_electrons_dphi",
+                              xtitle = "#Delta#phi(e_{lead},e_{sublead})",
+                              ytitle = "Events", 
+                              nbins  = 64,
+                              xmin   = -3.2,
+                              xmax   = 3.2,
+                              dir    = "event",
+                              vexpr  = "self.store['electrons_dphi']",
+                            )
+
+h_electrons_delta  = Hist1D( hname  = "h_electrons_delta",
+                              xtitle = "#Delta#eta(e_{lead},e_{sublead})",
+                              ytitle = "Events", 
+                              nbins  = 50,
+                              xmin   = -2.5,
+                              xmax   = 2.5,
+                              dir    = "event",
+                              vexpr  = "self.store['electrons_deta']",
+                            )
+
+h_electrons_mVis  = Hist1D( hname  = "h_electrons_mVis",
+                              xtitle = "m_{vis}(e_{lead},e_{sublead}) [GeV]",
+                              ytitle = "Events / (1 GeV)", 
+                              nbins  = 2000,
+                              xmin   = 0.0,
+                              xmax   = 2000.,
+                              dir    = "event",
+                              vexpr  = "self.store['mVis']/GeV",
+                            )
+
+h_electrons_mTtot  = Hist1D( hname  = "h_electrons_mTtot",
+                              xtitle = "m^{tot}_{T}(e_{lead},e_{sublead}) [GeV]",
+                              ytitle = "Events / (1 GeV)", 
+                              nbins  = 2000,
+                              xmin   = 0.0,
+                              xmax   = 2000.,
+                              dir    = "event",
+                              vexpr  = "self.store['mTtot']/GeV",
+                            )
+
+h_ejet_dphi  = Hist1D( hname  = "h_ejet_dphi",
+                              xtitle = "#Delta#phi(e_{lead},jet_{lead})",
+                              ytitle = "Events", 
+                              nbins  = 64,
+                              xmin   = -3.2,
+                              xmax   = 3.2,
+                              dir    = "event",
+                              vexpr  = "self.store['ejet_dphi']",
+)
+
+# ele lead
+# ------
+h_elelead_pt = Hist1D( hname  = "h_elelead_pt",
+                              xtitle = "p_{T}(e_{lead}) [GeV]",
+                              ytitle = "Events / (1 GeV)", 
+                              nbins  = 2000,
+                              xmin   = 0.0,
+                              xmax   = 2000.0,
+                              dir    = "electrons",
+                              vexpr  = "self.store['electrons'][0].tlv.Pt() / GeV",
+                            )
+
+h_elelead_eta = Hist1D( hname  = "h_elelead_eta",
+                              xtitle = "#eta(e_{lead})",
+                              ytitle = "Events / (0.1)", 
+                              nbins  = 50,
+                              xmin   = -2.5,
+                              xmax   = 2.5,
+                              dir    = "electrons",
+                              vexpr  = "self.store['electrons'][0].tlv.Eta()",
+                            )
+
+h_elelead_phi = Hist1D( hname  = "h_elelead_phi",
+                              xtitle = "#phi(e_{lead})",
+                              ytitle = "Events / (0.1)", 
+                              nbins  = 64,
+                              xmin   = -3.2,
+                              xmax   = 3.2,
+                              dir    = "electrons",
+                              vexpr  = "self.store['electrons'][0].tlv.Phi()",
+                            )
+
+h_elelead_trkd0 = Hist1D( hname  = "h_elelead_trkd0",
+                              xtitle = "d^{trk}_{0}(e_{lead}) [mm]",
+                              ytitle = "Events / (0.01)", 
+                              nbins  = 80,
+                              xmin   = -0.4,
+                              xmax   = 0.4,
+                              dir    = "electrons",
+                              vexpr  = "self.store['electrons'][0].trkd0",
+                            )
+
+h_elelead_trkd0sig = Hist1D( hname  = "h_elelead_trkd0sig",
+                              xtitle = "d^{trk}_{0} / #sigma(d^{trk}_{0}) (e_{lead})",
+                              ytitle = "Events / (0.01)", 
+                              nbins  = 100,
+                              xmin   = 0.,
+                              xmax   = 10.,
+                              dir    = "electrons",
+                              vexpr  = "self.store['electrons'][0].trkd0sig",
+                            )
+
+h_elelead_trkz0 = Hist1D( hname  = "h_elelead_trkz0",
+                              xtitle = "z^{trk}_{0}(e_{lead}) [mm]",
+                              ytitle = "Events / (0.01)", 
+                              nbins  = 40,
+                              xmin   = -2,
+                              xmax   = 2,
+                              dir    = "electrons",
+                              vexpr  = "self.store['electrons'][0].trkz0",
+                            )
+
+h_elelead_trkz0sintheta  = Hist1D( hname  = "h_elelead_trkz0sintheta",
+                              xtitle = "z^{trk}_{0}sin#theta(e_{lead}) [mm]",
+                              ytitle = "Events / (0.01)", 
+                              nbins  = 200,
+                              xmin   = -1,
+                              xmax   = 1,
+                              dir    = "electrons",
+                              vexpr  = "self.store['electrons'][0].trkz0sintheta",
+                            )
+
+h_elelead_ptvarcone30  = Hist1D( hname  = "h_elelead_ptvarcone30",
+                              xtitle = "ptvarcone30/p_{T}(e_{lead})",
+                              ytitle = "Events / (0.001)", 
+                              nbins  = 10000,
+                              xmin   = 0.,
+                              xmax   = 10.,
+                              dir    = "electrons",
+                              vexpr  = "self.store['electrons'][0].ptvarcone30 / self.store['electrons'][0].tlv.Pt()",
+                            )
+
+# elesublead
+# ---------
+h_elesublead_pt = Hist1D( hname  = "h_elesublead_pt",
+                              xtitle = "p_{T}(e_{sublead}) [GeV]",
+                              ytitle = "Events / (1 GeV)", 
+                              nbins  = 2000,
+                              xmin   = 0.0,
+                              xmax   = 2000.0,
+                              dir    = "electrons",
+                              vexpr  = "self.store['electrons'][1].tlv.Pt() / GeV",
+                            )
+
+h_elesublead_eta = Hist1D( hname  = "h_elesublead_eta",
+                              xtitle = "#eta(e_{sublead})",
+                              ytitle = "Events / (0.1)", 
+                              nbins  = 50,
+                              xmin   = -2.5,
+                              xmax   = 2.5,
+                              dir    = "electrons",
+                              vexpr  = "self.store['electrons'][1].tlv.Eta()",
+                            )
+
+h_elesublead_phi = Hist1D( hname  = "h_elesublead_phi",
+                              xtitle = "#phi(e_{sublead})",
+                              ytitle = "Events / (0.1)", 
+                              nbins  = 64,
+                              xmin   = -3.2,
+                              xmax   = 3.2,
+                              dir    = "electrons",
+                              vexpr  = "self.store['electrons'][1].tlv.Phi()",
+                            )
+
+h_elesublead_trkd0 = Hist1D( hname  = "h_elesublead_trkd0",
+                              xtitle = "d^{trk}_{0}(e_{sublead}) [mm]",
+                              ytitle = "Events / (0.01)", 
+                              nbins  = 80,
+                              xmin   = -0.4,
+                              xmax   = 0.4,
+                              dir    = "electrons",
+                              vexpr  = "self.store['electrons'][1].trkd0",
+                            )
+
+h_elesublead_trkd0sig = Hist1D( hname  = "h_elesublead_trkd0sig",
+                              xtitle = "d^{trk}_{0} / #sigma(d^{trk}_{0}) (e_{sublead})",
+                              ytitle = "Events / (0.01)", 
+                              nbins  = 100,
+                              xmin   = 0.,
+                              xmax   = 10.,
+                              dir    = "electrons",
+                              vexpr  = "self.store['electrons'][1].trkd0sig",
+                            )
+
+h_elesublead_trkz0 = Hist1D( hname  = "h_elesublead_trkz0",
+                              xtitle = "z^{trk}_{0}(e_{sublead}) [mm]",
+                              ytitle = "Events / (0.01)", 
+                              nbins  = 40,
+                              xmin   = -2,
+                              xmax   = 2,
+                              dir    = "electrons",
+                              vexpr  = "self.store['electrons'][1].trkz0",
+                            )
+
+h_elesublead_trkz0sintheta  = Hist1D( hname  = "h_elesublead_trkz0sintheta",
+                              xtitle = "z^{trk}_{0}sin#theta(e_{sublead}) [mm]",
+                              ytitle = "Events / (0.01)", 
+                              nbins  = 200,
+                              xmin   = -1,
+                              xmax   = 1,
+                              dir    = "electrons",
+                              vexpr  = "self.store['electrons'][1].trkz0sintheta",
+                            )
+
+h_elesublead_ptvarcone30  = Hist1D( hname  = "h_elesublead_ptvarcone30",
+                              xtitle = "ptvarcone30/p_{T}(e_{sublead})",
+                              ytitle = "Events / (0.001)", 
+                              nbins  = 10000,
+                              xmin   = 0.,
+                              xmax   = 10.,
+                              dir    = "electrons",
+                              vexpr  = "self.store['electrons'][1].ptvarcone30 / self.store['electrons'][1].tlv.Pt()",
+)
+
+# -------------
+# tag and probe
+# -------------
+h_eletag_pt = Hist1D( hname  = "h_eletag_pt",
+                              xtitle = "p_{T}(e_{tag}) [GeV]",
+                              ytitle = "Events / (1 GeV)", 
+                              nbins  = 2000,
+                              xmin   = 0.0,
+                              xmax   = 2000.0,
+                              dir    = "electrons",
+                              vexpr  = "self.store['tag'].tlv.Pt() / GeV",
+                            )
+
+h_eleprobe_pt = Hist1D( hname  = "h_eleprobe_pt",
+                              xtitle = "p_{T}(e_{probe}) [GeV]",
+                              ytitle = "Events / (1 GeV)", 
+                              nbins  = 2000,
+                              xmin   = 0.0,
+                              xmax   = 2000.0,
+                              dir    = "electrons",
+                              vexpr  = "self.store['probe'].tlv.Pt() / GeV",
+                            )
+
+h_eleprobe_ptiso = Hist1D( hname  = "h_eleprobe_ptiso",
+                              xtitle = "p_{T}(e_{probe}) + ptvarcone30 [GeV]",
+                              ytitle = "Events / (1 GeV)", 
+                              nbins  = 1000,
+                              xmin   = 0.0,
+                              xmax   = 1000.0,
+                              dir    = "electrons",
+                              vexpr  = "( self.store['probe'].tlv.Pt() + self.store['probe'].ptvarcone30 ) / GeV",
+                            )
+
+h_eleprobe_ujet_pt = Hist1D( hname  = "h_eleprobe_ujet_pt",
+                              xtitle = "p_{T}(e_{probe} underlying jet) [GeV]",
+                              ytitle = "Events / (1 GeV)", 
+                              nbins  = 2100,
+                              xmin   = -100.0,
+                              xmax   = 2000.0,
+                              dir    = "electrons",
+                              vexpr  = "self.store['probe_ujet_pt']",
+                            )
+
+h_eletag_ptvarcone30  = Hist1D( hname  = "h_eletag_ptvarcone30",
+                              xtitle = "ptvarcone30/p_{T}(e_{tag})",
+                              ytitle = "Events / (0.001)", 
+                              nbins  = 10000,
+                              xmin   = 0.,
+                              xmax   = 10.,
+                              dir    = "electrons",
+                              vexpr  = "self.store['tag'].ptvarcone30 / self.store['tag'].tlv.Pt()",
+                            )
+h_eleprobe_ptvarcone30  = Hist1D( hname  = "h_eleprobe_ptvarcone30",
+                              xtitle = "ptvarcone30/p_{T}(e_{probe})",
+                              ytitle = "Events / (0.001)", 
+                              nbins  = 10000,
+                              xmin   = 0.,
+                              xmax   = 10.,
+                              dir    = "electrons",
+                              vexpr  = "self.store['probe'].ptvarcone30 / self.store['probe'].tlv.Pt()",
+)
+
+# --------
+# 2D hists
+# --------
+h_elelead_ptiso_jetlead_pt  = Hist2D( hname      = "h_elelead_ptiso_jetlead_pt",
+                              xtitle  = "p_{T}(e_{lead}) + ptvarcone30 [GeV]",
+                              ytitle  = "p_{T}(jet_{lead}) [GeV]", 
+                              nbinsx  = 1000,
+                              xmin    = 0.,
+                              xmax    = 1000.,
+                              nbinsy  = 1000,
+                              ymin    = 0.,
+                              ymax    = 1000.,
+                              dir     = "event",
+                              vexpr   = "( self.store['electrons'][0].tlv.Pt() + self.store['electrons'][0].ptvarcone30 ) / GeV , self.store['jets'][0].tlv.Pt() / GeV",
+                          )
+
+
+h_elelead_pt_elelead_iso  = Hist2D( hname      = "h_elelead_pt_elelead_iso",
+                              xtitle  = "p_{T}(e_{lead}) [GeV]",
+                              ytitle  = "ptvarcone30(e_{lead}) [GeV]", 
+                              nbinsx  = 1000,
+                              xmin    = 0.,
+                              xmax    = 1000.,
+                              nbinsy  = 1000,
+                              ymin    = 0.,
+                              ymax    = 1000.,
+                              dir     = "event",
+                              vexpr   = "self.store['electrons'][0].tlv.Pt() / GeV , self.store['electrons'][0].ptvarcone30 / GeV",
+                          )
+
+h_elelead_pt_jetlead_pt  = Hist2D( hname      = "h_elelead_pt_jetlead_pt",
+                              xtitle  = "p_{T}(e_{lead}) [GeV]",
+                              ytitle  = "p_{T}(jet_{lead}) [GeV]", 
+                              nbinsx  = 1000,
+                              xmin    = 0.,
+                              xmax    = 1000.,
+                              nbinsy  = 1000,
+                              ymin    = 0.,
+                              ymax    = 1000.,
+                              dir     = "event",
+                              vexpr   = " self.store['electrons'][0].tlv.Pt() / GeV , self.store['jets'][0].tlv.Pt() / GeV",
+
+
 
 # EOF
